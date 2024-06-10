6d6963d44af4109d8603583e4b21dc3e2535a729
buiduc9918 <97076379+buiduc9918@users.noreply.github.com>
2024-06-10 10:30:43 +0700

    add

---
+++ Assets/Winner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Winner : MonoBehaviour
+{
+    public GameObject winner;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bird"))
+        {
+
+            winner.SetActive(true);
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Bird")) // Kiểm tra nếu đối tượng rời khỏi là Bird da roi khoi vung hoat dong
+        {
+            Debug.Log("Player exited trigger zone.");
+            winner.SetActive(false);
+        }
+    }
+}
